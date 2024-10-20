e2ebc07ab2925151d038117bde14646eb268cbaa
Nain Figueroa <nainzavala7@gmail.com>
2024-10-20 13:53:34 -0700

    Adiciones:     -Menu de inicio Cambios:

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 using namespace std;
 
+//=================MENU INICIAL================================================
+void menu()
+{
+    int i, a = 0;
+
+    do
+    {
+        // Color a las letras con "La secuencia de escape ANSI"
+        cout << "\033[1;32m------------------" << endl;
+        cout << "|      MENU     |" << endl;
+        cout << "------------------\033[0m " << endl;
+
+        cout << "==================" << endl;
+        cout << "1.-\tJUGAR" << endl;
+        cout << "0.-\tSALIR" << endl;
+        cout << "==================" << endl;
+
+        cout << "\033[1;37mOpcion a elegir ->\033[0m ";
+        cin >> i;
+
+        switch (i)
+        {
+        case 1:
+            cout << "Le dio a jugar" << endl;
+            a = 1;
+            break;
+        case 0:
+            cout << "Le dio a salir" << endl;
+            a = 1;
+            break;
+        default:
+            system("clear");
+            cout << "\033[1;31m!OPCION NO VALIDA¡\033[0m" << endl;
+            a = 0;
+            break;
+        }
+    } while (a != 1);
+}
+
 int main()
 {
-    cout << "Hola Mundo con Cmake:D";
+    menu();
+
     return 0;
 }
