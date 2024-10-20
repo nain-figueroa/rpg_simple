0b2c4b8edc9659f5cc526ac0f3ff73664d013e0e
Nain Figueroa <nainzavala7@gmail.com>
2024-10-20 12:55:16 -0700

    Adiciones:     Configuración del CMakeLists.txt Cambios:

---
+++ build/CMakeFiles/My_Rpg.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/My_Rpg.dir/src/main.cpp.o"
+  "CMakeFiles/My_Rpg.dir/src/main.cpp.o.d"
+  "My_Rpg"
+  "My_Rpg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/My_Rpg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/My_Rpg.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for My_Rpg.
+# This may be replaced when dependencies are built.
